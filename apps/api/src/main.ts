0e9455b056c131b8aca1384bd6c8444495d830dc
d60-wizard <116577684+d60-wizard@users.noreply.github.com>
2026-05-13 03:21:55 +0500

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,23 @@ import { ApiModule } from './api.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApiModule);
-	app.enableCors();
+	app.enableCors({
+	  origin: true,
+	  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+	  credentials: true,
+	  allowedHeaders: [
+	    'Origin',
+	    'X-Requested-With',
+	    'Content-Type',
+	    'Accept',
+	    'Authorization',
+	    'Access-Control-Allow-Headers',
+	    'Access-Control-Request-Method',
+	    'Access-Control-Request-Headers',
+	  ],
+	  preflightContinue: false,
+	  optionsSuccessStatus: 204,
+	});
 	await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
